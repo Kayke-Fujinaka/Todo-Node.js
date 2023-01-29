1f53b5ccce64510ca2288fefc757f63f3b4a5f0a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-29 20:05:34 -0300

    feat: creates middleware to check exists todo

--- src/index.js
+++ src/index.js
@@ -22,6 +22,19 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
+function checkExistsTodo(request, response, next) {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
+
+  request.todo = todo;
+
+  return next();
+}
+
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
@@ -66,24 +79,20 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
   return response.status(201).json(todo);
 });
 
-app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { user } = request;
-  const { title, deadline } = request.body;
-  const { id } = request.params;
+app.put(
+  "/todos/:id",
+  checksExistsUserAccount,
+  checkExistsTodo,
+  (request, response) => {
+    const { todo } = request;
+    const { title, deadline } = request.body;
 
-  const todo = user.todos.find((todo) => todo.id === id);
+    todo.title = title;
+    todo.deadline = new Date(deadline);
 
-  if (!todo) return response.status(404).json({ error: "Todo not found!" });
-
-  todo.title = title;
-  todo.deadline = new Date(deadline);
-
-  return response.json(todo);
-});
-
-app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    return response.json(todo);
+  }
+);
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
